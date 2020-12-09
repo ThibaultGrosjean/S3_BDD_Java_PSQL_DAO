27ae43b4f7ea12baf9c06ad857f01fc79bdcf748
Yassine SMARA <yassine.smara@laposte.net>
2020-12-09 16:56:16 +0100

    Ajout de TypeDAO et de testType

---
+++ src/dao/jdbc/TypeDaoImpl.java
@@ -0,0 +1,120 @@
+package dao.jdbc;
+import dao.exception.DaoException;
+import model.Entity;
+import model.Type;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TypeDaoImpl extends JdbcDao {
+
+    public TypeDaoImpl(Connection connection) {
+        super(connection);
+    }
+
+    @Override public Collection<Entity> findAll() throws DaoException {
+        Collection<Entity> types = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM TYPE;");
+
+            while (resultSet.next()) {
+                Type type = new Type();
+                type.setId(resultSet.getInt("idtype"));
+                type.setLibelle(resultSet.getString("libelletype"));
+                types.add(type);
+            }
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+
+        return types;
+    }
+
+    @Override
+    public Entity findById(int id) throws DaoException {
+        Type type = new Type();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM type WHERE idType ="+ id +";");
+
+            while (resultSet.next()) {
+                type.setId(resultSet.getInt("idtype"));
+                type.setLibelle(resultSet.getString("libelletype"));
+            }
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+        return type;
+    }
+
+    @Override
+    public void create(Entity entity) throws DaoException {
+        Type type = (Type) entity;
+
+        PreparedStatement stmt= null;
+        String sqlReq = "INSERT INTO TYPE(libelleType) VALUES (?) ;";
+
+        try {
+            stmt = connection.prepareStatement(sqlReq);
+            stmt.setString(1, type.getLibelle());
+
+            int res = stmt.executeUpdate();
+            if (res > 0) {
+                System.out.println("Ligne insérée");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erreur SQL : " + e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public void update(Entity entity) throws DaoException {
+        Type type = (Type) entity;
+
+        PreparedStatement stmt= null;
+        String sqlReq = "UPDATE TYPE set libelleType = (?) WHERE idType = (?) ;";
+
+        try {
+            stmt = connection.prepareStatement(sqlReq);
+            stmt.setString(1, type.getLibelle());
+            stmt.setInt(2, type.getId());
+
+
+            int res = stmt.executeUpdate();
+            if (res > 0) {
+                System.out.println("Ligne modifié");
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur SQL : " + e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public void delete(Entity entity) throws DaoException {
+        Type type = (Type) entity;
+
+        PreparedStatement stmt= null;
+
+        String sqlReq = "DELETE FROM type WHERE idtype = (?);";
+
+        try {
+
+            stmt = connection.prepareStatement(sqlReq);
+            stmt.setInt(1, type.getId());
+
+            int res = stmt.executeUpdate();
+            if (res > 0) {
+                System.out.println("Ligne supprimé");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erreur SQL : " + e.getLocalizedMessage());
+
+        }
+    }
+
+}
---
+++ src/test/SimpleJdbcDaoTestType.java
@@ -0,0 +1,107 @@
+package test;
+
+import dao.Dao;
+import dao.jdbc.TypeDaoImpl;
+import dao.exception.DaoException;
+import model.Entity;
+import model.Type;
+import sql.PostgresConnection;
+
+import java.sql.Connection;
+import java.util.Collection;
+
+public class SimpleJdbcDaoTestType {
+    private Connection connection;
+    private Dao dao;
+
+    public void testfindAllTypes() {
+        Dao dao = new TypeDaoImpl(connection);
+
+        try {
+            Collection<Entity> types = dao.findAll();
+            for (Entity entity : types) {
+                Type type = (Type) entity;
+                System.out.println(type.getId() + " | " + type.getLibelle());
+            }
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testfindByIdTypes(int id) {
+        Dao dao = new TypeDaoImpl(connection);
+
+        try {
+            Type type = (Type) dao.findById(id);
+            System.out.println(type.getId() + " | " + type.getLibelle());
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testCreateType(Type type) {
+        try {
+            dao.create(type);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void testEditType(Type type) {
+        try {
+            dao.update(type);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void testDeleteType(Type type) {
+        try {
+            dao.delete(type);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void test() {
+        connection = PostgresConnection.getInstance();
+        dao = new TypeDaoImpl(connection);
+        testfindAllTypes();
+
+        Type type = new Type("TYPE");
+
+        //create
+        System.out.println("\n***** Création d'une type : ");
+        testCreateType(type);
+
+        //find all
+        System.out.println("\n***** Liste des types : ");
+        testfindAllTypes();
+
+        //find by id
+        System.out.println("\n***** Type 5 : ");
+        testfindByIdTypes(5);
+
+
+        //edit
+        System.out.println("\n***** Modification d'une type : ");
+        testEditType(
+                new Type(1,"TYPEEDIT")
+        );
+        testfindAllTypes();
+
+        //delete
+        System.out.println("\n***** Suppression d'une type : ");
+        type.setId(5);
+        testDeleteType(type);
+        testfindAllTypes();
+    }
+
+
+    public static void main(String[] args) {
+        new SimpleJdbcDaoTestType().test();
+    }
+}
