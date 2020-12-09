c9b413ab19e2f42b22c72318517c87c80fd83622
Yassine SMARA <yassine.smara@laposte.net>
2020-12-09 17:58:17 +0100

    Le vrai Ajout de ClientDAO et de testDAO

---
+++ src/dao/jdbc/ClientDaoImpl.java
@@ -0,0 +1,135 @@
+package dao.jdbc;
+import dao.exception.DaoException;
+import model.Entity;
+import model.Client;
+import model.Ville;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ClientDaoImpl extends JdbcDao {
+
+    private VilleDaoImpl villeDao ;
+
+    public ClientDaoImpl(Connection connection) {
+        super(connection);
+        villeDao = new VilleDaoImpl(connection);
+    }
+
+    @Override public Collection<Entity> findAll() throws DaoException {
+        Collection<Entity> clients = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM client");
+
+            while (resultSet.next()) {
+                Client client = new Client();
+                client.setId(resultSet.getInt("idclient"));
+                client.setNom(resultSet.getString("nomClient"));
+                client.setAdresse(resultSet.getString("adresseClient"));
+                client.setCodePostale(resultSet.getInt("codePostalCLient"));
+                client.setVille((Ville) villeDao.findById(resultSet.getInt("idville")));
+                clients.add(client);
+            }
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+
+        return clients;
+    }
+
+    @Override
+    public Entity findById(int id) throws DaoException {
+        Client client = new Client();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM client WHERE idClient ="+ id +";");
+
+            while (resultSet.next()) {
+                client.setId(resultSet.getInt("idclient"));
+                client.setNom(resultSet.getString("nomClient"));
+                client.setAdresse(resultSet.getString("adresseClient"));
+                client.setCodePostale(resultSet.getInt("codePostalCLient"));
+                client.setVille((Ville) villeDao.findById(resultSet.getInt("idville")));
+            }
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+        return client;
+    }
+
+    @Override
+    public void create(Entity entity) throws DaoException {
+        Client client = (Client) entity;
+
+        PreparedStatement stmt= null;
+        String sqlReq = "INSERT INTO CLIENT(nomClient, adresseClient, codePostalCLient, idVille) VALUES ((?),(?), (?),(?)) ;\n";
+
+        try {
+            stmt = connection.prepareStatement(sqlReq);
+            stmt.setString(1, client.getNom());
+            stmt.setString(2, client.getAdresse());
+            stmt.setInt(3, client.getCodePostale());
+            stmt.setInt(4, client.getVille().getId());
+
+            int res = stmt.executeUpdate();
+            if (res > 0) {
+                System.out.println("Ligne insérée");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erreur SQL : " + e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public void update(Entity entity) throws DaoException {
+        Client client = (Client) entity;
+
+        PreparedStatement stmt= null;
+        String sqlReq = "UPDATE CLIENT set nomClient = (?) ,adresseClient = (?) ,codePostalCLient = (?) , idVille = (?) WHERE idClient = (?) ;\n";
+
+        try {
+            stmt = connection.prepareStatement(sqlReq);
+            stmt.setString(1, client.getNom());
+            stmt.setString(2, client.getAdresse());
+            stmt.setInt(3, client.getCodePostale());
+            stmt.setInt(4, client.getVille().getId());
+            stmt.setInt(5, client.getId());
+
+            int res = stmt.executeUpdate();
+            if (res > 0) {
+                System.out.println("Ligne modifié");
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur SQL : " + e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public void delete(Entity entity) throws DaoException {
+        Client client = (Client) entity;
+
+        PreparedStatement stmt= null;
+
+        String sqlReq = "DELETE FROM client WHERE idclient = ?;";
+
+        try {
+
+            stmt = connection.prepareStatement(sqlReq);
+            stmt.setInt(1, client.getId());
+
+            int res = stmt.executeUpdate();
+            if (res > 0) {
+                System.out.println("Ligne supprimé");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erreur SQL : " + e.getLocalizedMessage());
+
+        }
+    }
+
+}
---
+++ src/test/SimpleJdbcDaoTestClient.java
@@ -0,0 +1,107 @@
+package test;
+
+import dao.Dao;
+import dao.jdbc.ClientDaoImpl;
+import dao.exception.DaoException;
+import model.Entity;
+import model.Client;
+import model.Ville;
+import sql.PostgresConnection;
+
+import java.sql.Connection;
+import java.util.Collection;
+
+public class SimpleJdbcDaoTestClient {
+    private Connection connection;
+    private Dao dao;
+
+    public void testfindAllClients() {
+        Dao dao = new ClientDaoImpl(connection);
+
+        try {
+            Collection<Entity> clients = dao.findAll();
+            for (Entity entity : clients) {
+                Client client = (Client) entity;
+                System.out.println(client.getId() + " | " + client.getNom() + "|" + " | " + client.getAdresse() + " | " + client.getCodePostale() + client.getVille().getId() + "|"+ client.getVille().getNom() + "|"+ client.getVille().getNbHabitant() + "|");
+            }
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testfindByIdClients(int id) {
+        Dao dao = new ClientDaoImpl(connection);
+
+        try {
+            Client client = (Client) dao.findById(id);
+            System.out.println(client.getId() + " | " + client.getNom() + "|" + " | " + client.getAdresse() + " | " + client.getCodePostale() + client.getVille().getId() + "|"+ client.getVille().getNom() + "|"+ client.getVille().getNbHabitant() + "|");
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testCreateClient(Client client) {
+        try {
+            dao.create(client);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void testEditClient(Client client) {
+        try {
+            dao.update(client);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void testDeleteClient(Client client) {
+        try {
+            dao.delete(client);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void test() {
+        connection = PostgresConnection.getInstance();
+        dao = new ClientDaoImpl(connection);
+        testfindAllClients();
+
+        Client client = new Client(6,"CLIENT","7depihped", 25200);
+        client.setVille(new Ville(1));
+
+        //create
+        System.out.println("\n***** Création d'une client : ");
+        testCreateClient(client);
+
+        //find all
+        System.out.println("\n***** Liste des clients : ");
+        testfindAllClients();
+
+        //find by id
+        System.out.println("\n***** Client 5 : ");
+        testfindByIdClients(5);
+
+
+        //edit
+        System.out.println("\n***** Modification d'une client : ");
+        client.setNom("CLIENTEDIT");
+        testEditClient(client);
+        testfindAllClients();
+
+        //delete
+        System.out.println("\n***** Suppression d'une client : ");
+        testDeleteClient(client);
+        testfindAllClients();
+    }
+
+
+    public static void main(String[] args) {
+        new SimpleJdbcDaoTestClient().test();
+    }
+}
